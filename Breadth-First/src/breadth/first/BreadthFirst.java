2955ce0f5fd9b949d90093a6a99066412f2dd391
RecordBreaker12 <50182908+RecordBreaker12@users.noreply.github.com>
2021-06-23 19:29:15 -0300

    Add files via upload

---
+++ Breadth-First/src/breadth/first/BreadthFirst.java
@@ -0,0 +1,59 @@
+package breadth.first;
+
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Queue;
+import java.util.Set;
+
+public class BreadthFirst {
+    
+    public static <T> Optional<No<T>> procura(T valor, No<T> inicio){
+        Queue<No<T>> fila = new ArrayDeque<>();
+        fila.add(inicio);
+        
+        No<T> atual;
+        Set<No<T>> jaVisitou = new HashSet<>();
+        
+        while(!fila.isEmpty()){
+            atual = fila.remove();
+            System.out.println("Visitou no com valor: " + atual.getValor());
+            if(atual.getValor().equals(valor)){
+                return Optional.of(atual);
+            }
+            else{
+                jaVisitou.add(atual);
+                fila.addAll(atual.getVizinhos());
+                fila.removeAll(jaVisitou);
+            }
+        }
+        return Optional.empty();
+    }
+    
+    public static void main(String[] args) {
+        No no0 = new No(0);
+        No no1 = new No(1);
+        No no2 = new No(2);
+        No no3 = new No(3);
+        No no4 = new No(4);
+        No no5 = new No(5);
+        No no6 = new No(6);
+        No no7 = new No(7);
+        no0.conectar(no2);
+        no0.conectar(no3);
+        no0.conectar(no4);
+        no1.conectar(no5);
+        no2.conectar(no4);
+        no3.conectar(no1);
+        no3.conectar(no5);
+        no4.conectar(no2);
+        no4.conectar(no7);
+        no5.conectar(no1);
+        no5.conectar(no3);
+        no5.conectar(no7);
+        no6.conectar(no4);
+        no6.conectar(no5);
+        procura(7, no0);
+    }
+    
+}
